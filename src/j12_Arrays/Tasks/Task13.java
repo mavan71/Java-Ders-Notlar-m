20cedb2d58b7aee52c015bbb9561c38661c96c8a
Murat AVAN <mavan71@gmail.com>
2023-02-21 14:35:17 +0300

    2

--- src/j12_Arrays/Tasks/Task13.java
+++ src/j12_Arrays/Tasks/Task13.java
@@ -2,6 +2,8 @@ package j12_Arrays.Tasks;
 
 import java.util.Scanner;
 
+import static java.sql.DriverManager.println;
+
 public class Task13 {
 
     public static void main(String[] args) {
@@ -10,6 +12,7 @@ public class Task13 {
         3 e bolunebilen sayi adedini print eden  code create ediniz (negatif sayilar dahil )
 
          */
+System.out.println("merhaba");
 
 
 
